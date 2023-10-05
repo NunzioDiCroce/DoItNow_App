c2f19252d1ffd3ce1150ab51079266c69fd9780a
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-05 17:17:41 +0200

    Create Project

---
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/Projects/DoItNow_BackEnd_App/DoItNowBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package Projects.DoItNow_BackEnd_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItNowBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoItNowBackEndAppApplication.class, args);
+	}
+
+}
---
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/test/java/Projects/DoItNow_BackEnd_App/DoItNowBackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Projects.DoItNow_BackEnd_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItNowBackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

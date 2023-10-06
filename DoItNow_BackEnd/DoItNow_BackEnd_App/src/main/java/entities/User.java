6481f0efa88bcb1cef70fe08ce1ebbfa8f69df6d
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-06 10:53:21 +0200

    Update User.java

--- DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/entities/User.java
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/entities/User.java
@@ -1,9 +1,20 @@
 package entities;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import enums.Role;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -14,17 +25,75 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 @Data
 @NoArgsConstructor
-public class User {
+@JsonIgnoreProperties({ "password", "authorities", "accountNonExpired", "accountNonLocked", "credentialsNonExpired" })
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue
 	private UUID id;
 
+	@Column(nullable = false)
 	private String name;
+
+	@Column(nullable = false)
 	private String surname;
+
+	@Column(nullable = false)
 	private String userName;
-	private String mail;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
 	private String password;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
+
+	public User(String name, String surname, String userName, String email, String password) {
+		this.name = name;
+		this.surname = surname;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.role = Role.USER;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
 }

4325a446d0c5849b61c4f994dc331bd1654abf66
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-06 11:11:03 +0200

    Update User.java

--- DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/entities/User.java
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/entities/User.java
@@ -19,12 +19,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
-@NoArgsConstructor
 @JsonIgnoreProperties({ "password", "authorities", "accountNonExpired", "accountNonLocked", "credentialsNonExpired" })
 public class User implements UserDetails {
 

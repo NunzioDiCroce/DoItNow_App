f84e1c2d6c3f46d8660111e5ff72a5f8bc15f2a6
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-09 22:59:39 +0200

    Create TaskService.java

---
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/payloads/TaskRequestPayload.java
@@ -0,0 +1,38 @@
+package payloads;
+
+import java.time.LocalDate;
+
+import entities.User;
+import enums.Category;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class TaskRequestPayload {
+
+	private String taskId;
+
+	@NotNull(message = "The title is required.")
+	@Size(min = 2, max = 30, message = "The title must have a minimum of 2 characters, a maximum of 30.")
+	private String title;
+
+	@NotNull(message = "The description is required.")
+	@Size(min = 2, max = 30, message = "The description must have a minimum of 2 characters, a maximum of 30.")
+	private String description;
+
+	@NotNull(message = "The category is required.")
+	private Category category;
+
+	@NotNull(message = "The expiration date is required.")
+	private LocalDate expirationDate;
+
+	private Boolean completed;
+
+	@Size(min = 2, max = 30, message = "The notes must have a minimum of 2 characters, a maximum of 30.")
+	private String notes;
+
+	@NotNull(message = "The user is required.")
+	private User user;
+
+}
--- DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/services/TaskService.java
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/services/TaskService.java
@@ -1,8 +1,48 @@
 package services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import entities.Task;
+import payloads.TaskRequestPayload;
+import repositories.TaskRepository;
 
 @Service
 public class TaskService {
+
+	private final TaskRepository taskRepository;
+
+	@Autowired
+	public TaskService(TaskRepository taskRepository) {
+		this.taskRepository = taskRepository;
+	}
+
+	// * * * * * * * * * * create task * * * * * * * * * *
+	@Transactional
+	public ResponseEntity<String> createTask(TaskRequestPayload body) {
+
+		Task newTask = new Task();
+
+		// other properties
+		newTask.setTitle(body.getTitle());
+		newTask.setDescription(body.getDescription());
+		newTask.setCategory(body.getCategory());
+		newTask.setExpirationDate(body.getExpirationDate());
+		newTask.setCompleted(false);
+		newTask.setNotes(body.getNotes());
+
+		// user
+		// TODO
+
+		// taskId
+		// TODO
+
+		taskRepository.save(newTask);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body("Task created successfully.");
+	}
 
 }

3524bbff3afb41dd25d6a9cfbbccfb2fbdfc9563
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-09 23:30:58 +0200

    Update TaskService.java

--- DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/repositories/TaskRepository.java
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/repositories/TaskRepository.java
@@ -2,6 +2,8 @@ package repositories;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,17 @@ import entities.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+	Page<Task> findAllByTaskIdContainingIgnoreCase(String taskId, Pageable pageable);
+
+	Page<Task> findAllByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+	Page<Task> findAllByDescriptionContainingIgnoreCase(String description, Pageable pageable);
+
+	Page<Task> findAllByCategoryContainingIgnoreCase(String category, Pageable pageable);
+
+	Page<Task> findAllByExpirationDateContainingIgnoreCase(String expirationDate, Pageable pageable);
+
+	Page<Task> findAllByCompletedContainingIgnoreCase(String completed, Pageable pageable);
 
 }
--- DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/services/TaskService.java
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/services/TaskService.java
@@ -1,6 +1,9 @@
 package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -44,5 +47,87 @@ public class TaskService {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body("Task created successfully.");
 	}
+
+	// * * * * * * * * * * find tasks by taskId (with pagination) * * * * * * * * *
+	@Transactional
+	public ResponseEntity<?> findTasksByTaskId(String taskId, int page, int size, String sort) {
+		Page<Task> tasks = taskRepository.findAllByTaskIdContainingIgnoreCase(taskId,
+				PageRequest.of(page, size, Sort.by(taskId)));
+
+		if (tasks.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tasks not found for task id: " + taskId);
+		} else {
+			return ResponseEntity.ok(tasks);
+		}
+	}
+
+	// * * * * * * * * * * find tasks by title (with pagination) * * * * * * * * * *
+	@Transactional
+	public ResponseEntity<?> findTasksByTitle(String title, int page, int size, String sort) {
+		Page<Task> tasks = taskRepository.findAllByTitleContainingIgnoreCase(title,
+				PageRequest.of(page, size, Sort.by(title)));
+
+		if (tasks.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tasks not found for title: " + title);
+		} else {
+			return ResponseEntity.ok(tasks);
+		}
+	}
+
+	// * * * * * * * * * * find tasks by description (with pagination) * * * * * * *
+	@Transactional
+	public ResponseEntity<?> findTasksByDescription(String description, int page, int size, String sort) {
+		Page<Task> tasks = taskRepository.findAllByDescriptionContainingIgnoreCase(description,
+				PageRequest.of(page, size, Sort.by(description)));
+
+		if (tasks.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tasks not found for description: " + description);
+		} else {
+			return ResponseEntity.ok(tasks);
+		}
+	}
+
+	// * * * * * * * * * * find tasks by category (with pagination) * * * * * * * *
+	@Transactional
+	public ResponseEntity<?> findTasksByCategory(String category, int page, int size, String sort) {
+		Page<Task> tasks = taskRepository.findAllByCategoryContainingIgnoreCase(category,
+				PageRequest.of(page, size, Sort.by(category)));
+
+		if (tasks.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tasks not found for category: " + category);
+		} else {
+			return ResponseEntity.ok(tasks);
+		}
+	}
+
+	// * * * * * * * * * * find tasks by expiration date (with pagination) * * * * *
+	@Transactional
+	public ResponseEntity<?> findTasksByExpirationDate(String expirationDate, int page, int size, String sort) {
+		Page<Task> tasks = taskRepository.findAllByExpirationDateContainingIgnoreCase(expirationDate,
+				PageRequest.of(page, size, Sort.by(expirationDate)));
+
+		if (tasks.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body("Tasks not found for expiration date: " + expirationDate);
+		} else {
+			return ResponseEntity.ok(tasks);
+		}
+	}
+
+	// * * * * * * * * * * find tasks by completed (with pagination) * * * * * * * *
+	@Transactional
+	public ResponseEntity<?> findTasksByCompleted(String completed, int page, int size, String sort) {
+		Page<Task> tasks = taskRepository.findAllByCompletedContainingIgnoreCase(completed,
+				PageRequest.of(page, size, Sort.by(completed)));
+
+		if (tasks.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tasks not found for completed: " + completed);
+		} else {
+			return ResponseEntity.ok(tasks);
+		}
+	}
+
+	// * * * * * * * * * * find tasks by user (with pagination) * * * * * * * * * *
+	// TODO
 
 }

0bd7a3781d83ee9091fec2d463d4cd71cfea67be
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-21 16:57:50 +0200

    Create routes (work in progress)

--- DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/app.module.ts
+++ DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// - - - - - - - - - - import - - - - - - - - - -
+import { Route, RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+// - - - - - - - - - - routes configuration - - - - - - - - - -
+const routes: Route[] = [
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+
+
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // - - - - - - - - - - imports - - - - - - - - - -
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]

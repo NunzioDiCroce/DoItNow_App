91041ee2a9dd42b7274201c58ac2595658660eab
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-23 12:29:42 +0200

    Complete auth.guard.ts

---
+++ DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, take } from 'rxjs';
+
+// - - - - - - - - - - import - - - - - - - - - -
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  // - - - - - - - - - - constructor definition - - - - - - - - - -
+  constructor(private authSrv: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    // - - - - - - - - - - AuthGuard definition - - - - - - - - - -
+    return this.authSrv.user$.pipe(take(1), map((user) => {
+      if(user) {
+        return true
+      }
+      alert('Login to access this resource.\nRegister if you have not an account.')
+      return this.router.createUrlTree(['/login'])
+    }));
+  }
+
+}

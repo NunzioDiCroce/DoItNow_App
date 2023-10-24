11cdd73be106c034128a64151c2798c3b595a934
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-24 17:29:13 +0200

    Complete register.component.ts

--- DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/auth/register/register.component.ts
+++ DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/auth/register/register.component.ts
@@ -19,17 +19,21 @@ export class RegisterComponent implements OnInit {
   }
 
   register(form: NgForm) {
-      this.authSrv.register(form.value).subscribe(() => {
-        window.alert('Registration success!');
-        this.router.navigate(['/login']);
-      },
-      (error) => {
-        switch(error.error) {
-          case 'The email has already been used.':
-          window.alert('The email has already been used.');
-          break
-        }
-      });
+    this.authSrv.register(form.value).subscribe(() => {
+      window.alert('Registration success!');
+      this.router.navigate(['/login']);
+    },
+    (error) => {
+      if(error.error && error.error.errorsList && error.error.errorsList.length > 0) {
+        let errorsMessage: string = 'Registration failed. Errors:';
+        error.error.errorsList.forEach((errorMessage: string) => {
+          errorsMessage += `\n - ${errorMessage}`
+        });
+        window.alert(errorsMessage);
+      } else {
+        window.alert('An error occurred while processing your request.');
+      }
+    });
   }
 
 }

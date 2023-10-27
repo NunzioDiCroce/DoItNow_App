4072ab4024294df38c8b631450aa44062d915e52
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-27 08:46:12 -0400

    Update login.component

--- DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/auth/login/login.component.ts
+++ DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// - - - - - - - - - - import - - - - - - - - - -
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // - - - - - - - - - - LoginComponent definition - - - - - - - - - -
+  isLoading = false;
+
+  constructor(private authSrv: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(form: NgForm) {
+    this.isLoading = true;
+    try {
+      this.authSrv.login(form.value).subscribe(() => {
+        this.isLoading = false;
+        alert('Registration success!');
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        this.isLoading = false;
+        alert('Wrong login!');
+        console.log(error);
+      });
+    } catch(error) {
+
+    }
   }
 
 }

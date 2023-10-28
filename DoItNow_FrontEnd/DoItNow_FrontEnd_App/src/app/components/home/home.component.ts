1c57cb9be6384a2a7093594d01a6ef9fff3877aa
NunzioDiCroce <nunziodavide@gmail.com>
2023-10-28 09:00:52 -0400

    Update home.component

--- DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/components/home/home.component.ts
+++ DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/components/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// - - - - - - - - - - import - - - - - - - - - -
+import { Router } from '@angular/router';
+import { AuthData } from 'src/app/auth/auth-data.interface';
+import { AuthService } from 'src/app/auth/auth.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // - - - - - - - - - - HomeComponent definition - - - - - - - - - -
+  user!: AuthData | null;
+
+  constructor(private authSrv: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.authSrv.user$.subscribe((_user) => {
+      this.user = _user;
+    });
+  }
+
+  goToRegister() {
+    this.router.navigate(['register']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['login']);
   }
 
 }

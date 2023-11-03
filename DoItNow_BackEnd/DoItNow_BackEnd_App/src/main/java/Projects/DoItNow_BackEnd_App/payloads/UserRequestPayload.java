d4449a327ff01c723914a599fff2658098b4ae42
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-04 00:51:59 +0100

    Update UserRequestPayload.java

--- DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/Projects/DoItNow_BackEnd_App/payloads/UserRequestPayload.java
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/Projects/DoItNow_BackEnd_App/payloads/UserRequestPayload.java
@@ -3,9 +3,13 @@ package Projects.DoItNow_BackEnd_App.payloads;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserRequestPayload {
 
 	@NotNull(message = "The name is required.")

c17928a082118795b5d2d101afa38aee11f4a8e4
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-03 16:12:14 +0100

    Update task-create.component

--- DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/components/task-create/task-create.component.ts
+++ DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/components/task-create/task-create.component.ts
@@ -4,6 +4,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthData } from 'src/app/auth/auth-data.interface';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import { NgForm } from '@angular/forms';
+import { TasksService } from 'src/app/services/tasks.service';
+import { TaskCreate } from 'src/app/models/task-create.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-create',
@@ -15,8 +19,18 @@ export class TaskCreateComponent implements OnInit, OnDestroy { // add OnDestroy
   // - - - - - - - - - - TaskCreateComponent definition - - - - - - - - - -
   user!: AuthData | null;
   authSub!: Subscription | null;
+  createTaskSub: Subscription | undefined;
 
-  constructor(private authSrv: AuthService) { }
+  // task initialization
+  task: TaskCreate = {
+    title: '',
+    description: '',
+    category: '',
+    expirationDate: new Date(),
+    notes: ''
+  };
+
+  constructor(private authSrv: AuthService, private tasksSrv: TasksService, private router: Router) { }
 
   ngOnInit(): void {
     this.authSub = this.authSrv.user$.subscribe((_user) => {
@@ -24,9 +38,28 @@ export class TaskCreateComponent implements OnInit, OnDestroy { // add OnDestroy
     });
   }
 
+  // createTask
+  createTask(form: NgForm): void {
+    this.createTaskSub = this.tasksSrv.createTask(this.task).subscribe(() => {
+      window.alert('Task creation success!')
+      this.router.navigate(['/tasks']);
+    });
+  }
+
+  // cancelCreation
+  cancelCreation(): void {
+    const confirmation = window.confirm('Are you sure you want to cancel the current operation?');
+    if(confirmation) {
+      this.router.navigate(['/tasks']);
+    }
+  }
+
   ngOnDestroy(): void {
     if(this.authSub) {
       this.authSub.unsubscribe();
+    }
+    if(this.createTaskSub) {
+      this.createTaskSub.unsubscribe();
     }
   }
 

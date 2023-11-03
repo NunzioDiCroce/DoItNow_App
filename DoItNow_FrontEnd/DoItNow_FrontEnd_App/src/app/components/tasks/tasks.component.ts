50e63be9822ce776021eb50f80e4471870526069
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-03 10:32:54 +0100

    Update tasks.component.ts

--- DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/components/tasks/tasks.component.ts
+++ DoItNow_FrontEnd/DoItNow_FrontEnd_App/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 // - - - - - - - - - - import - - - - - - - - - -
 import { AuthData } from 'src/app/auth/auth-data.interface';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/models/task.interface';
 import { AuthService } from 'src/app/auth/auth.service';
 import { TasksService } from 'src/app/services/tasks.service';
@@ -11,11 +12,13 @@ import { TasksService } from 'src/app/services/tasks.service';
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, OnDestroy { // add OnDestroy
 
   // - - - - - - - - - - TasksComponent definition - - - - - - - - - -
   user!: AuthData | null;
+  authSub!: Subscription | null;
   tasks: Task[] | undefined;
+  tasksSub: Subscription | undefined;
 
   // tasks pagination
   page: number = 0;
@@ -23,11 +26,13 @@ export class TasksComponent implements OnInit {
   sortBy: string = 'taskId';
   totalPages: number = 0;
   totalPagesArray: number[] = [];
+  pageIndex: number = 0;
+  pageFirstRow: number = 1;
 
   constructor(private authSrv: AuthService, private tasksSrv: TasksService) { }
 
   ngOnInit(): void {
-    this.authSrv.user$.subscribe((_user) => {
+    this.authSub = this.authSrv.user$.subscribe((_user) => {
       this.user = _user;
     });
     this.loadTasks(); // loadTasks with pagination
@@ -35,11 +40,47 @@ export class TasksComponent implements OnInit {
 
   // loadTasks with pagination
   loadTasks(): void {
-    this.tasksSrv.getTasks(this.page, this.size, this.sortBy).subscribe((pageData: any) => {
+    this.tasksSub = this.tasksSrv.getTasks(this.page, this.size, this.sortBy).subscribe((pageData: any) => {
       this.tasks = pageData.content;
       this.totalPages = pageData.totalPages;
       this.totalPagesArray = Array(this.totalPages).map((i) => i);
     });
+  }
+
+  prevPage(): void {
+    if(this.page > 0) {
+      this.page --;
+      this.pageIndex = this.page;
+      this.pageFirstRow = this.page * this.size + 1;
+      this.loadTasks();
+    }
+  }
+
+  nextPage(): void {
+    if(this.page < this.totalPages - 1) {
+      this.page ++;
+      this.pageIndex = this.page;
+      this.pageFirstRow = this.page * this.size + 1;
+      this.loadTasks();
+    }
+  }
+
+  goToPage(_page: number): void {
+    if(_page >= 0 && _page < this.totalPages) {
+      this.page = _page;
+      this.pageIndex = this.page;
+      this.pageFirstRow = this.page * this.size + 1;
+      this.loadTasks();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if(this.authSub) {
+      this.authSub.unsubscribe();
+    }
+    if(this.tasksSub) {
+      this.tasksSub.unsubscribe();
+    }
   }
 
 }

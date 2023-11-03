2c243caebccb67b71654eaddb4c671526cc36047
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-03 17:22:54 +0100

    Update Task.java

--- DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/Projects/DoItNow_BackEnd_App/entities/Task.java
+++ DoItNow_BackEnd/DoItNow_BackEnd_App/src/main/java/Projects/DoItNow_BackEnd_App/entities/Task.java
@@ -10,14 +10,21 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tasks")
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Task {
 
 	@Id
@@ -46,7 +53,6 @@ public class Task {
 	private String notes;
 
 	@ManyToOne
-	@JoinColumn(name = "user_id")
 	private User user;
 
 }
